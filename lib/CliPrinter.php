96d8652fc67c72cfeac06175daa0fb97975dc976
Farzin Bidokhti <farzin.bidokhti@gmail.com>
2023-04-06 10:01:24 +0330

    Init commit

---
+++ lib/CliPrinter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Minicli;
+
+class CliPrinter
+{
+    public function out($message)
+    {
+        echo $message;
+    }
+
+    public function newline()
+    {
+        $this->out("\n");
+    }
+
+    public function display($message)
+    {
+        $this->newline();
+        $this->out($message);
+        $this->newline();
+    }
+}
